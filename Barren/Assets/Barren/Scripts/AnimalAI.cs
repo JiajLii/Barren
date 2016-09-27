24c1bc49c808252579667dc3aeb205efabdc52d7
LiJiajun <719263840@qq.com>
2016-09-27 21:42:49 +0800

    no message

---
+++ Barren/Assets/Barren/Scripts/AnimalAI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface AnimalAI
+{
+	/// <summary>
+	/// 巡逻的方法
+	/// </summary>
+	void Patrol ();
+
+	/// <summary>
+	/// 追击的方法
+	/// </summary>
+	void Pursue ();
+
+	/// <summary>
+	/// 攻击策略的选择
+	/// </summary>
+	void Attack ();
+
+	/// <summary>
+	/// 死亡方法
+	/// </summary>
+	void Dead ();
+
+	/// <summary>
+	/// 物品掉落方法
+	/// </summary>
+	void FallingItems ();
+}
---
+++ Barren/Assets/Barren/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface Player
+{
+	/// <summary>
+	/// 返回你当前武器的攻击力
+	/// </summary>
+	void Attack ();
+
+	/// <summary>
+	/// 收到伤害的方法
+	/// </summary>
+	void UnderAttack (float damage);
+	
+}
